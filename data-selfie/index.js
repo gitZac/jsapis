51e47bda4368421340a262e6cafdaf8795972f29
Zac Taylor <ztaylor555@gmail.com>
2021-01-09 11:44:14 -0500

    data

--- data-selfie/index.js
+++ data-selfie/index.js
@@ -1,5 +1,22 @@
 const express = require('express');
 const app = express();
 
-app.listen(3030, () => console.log('Listening at 3030') );
+app.listen(3000, () => console.log('Listening at 3000') );
 app.use(express.static('public'));
+app.use(express.json({limit: '1mb'}));
+
+app.post('/api', (request, response) => {
+    console.log('REQUEST RECEIVED with the client data below: ');
+    //Log the request received from the client to the console.
+    console.log(request.body);
+
+    //Store the body of the request object in a variable.
+    const data = request.body;
+    
+    //Sends a json object RESPONSE that we created back to client with the lat lng data from the REQUEST.
+    response.json({
+        status: 'Success!',
+        latitude: data.lat,
+        longitude: data.lng
+    });
+}); 
--- data-selfie/public/src/js/main.js
+++ data-selfie/public/src/js/main.js
@@ -1,9 +1,38 @@
-
 if ('geolocation' in navigator){
     console.log('Geolocation available');
-    navigator.geolocation.getCurrentPosition(position => {
-        console.log(position);
+    navigator.geolocation.getCurrentPosition(async position => {
+        console.log(position.coords.latitude);
+        console.log(position.coords.longitude);
+
+        lat = position.coords.latitude;
+        lng = position.coords.longitude;
+        document.getElementById('Latitude').textContent = lat;
+        document.getElementById('Longitude').textContent = lng;
+        
+        // Store the latitude and longitude in an object that we will send to the server.
+        const data = {lat, lng};
+
+        //Create an object called options for the POST that we are sending to the server.
+        const options = {
+            method: 'POST',
+            headers:{
+                'Content-Type': 'application/json'
+            },
+            // Convert the body of the POST to a JSON string
+            body: JSON.stringify(data)
+        }
+        //Fetch the RESPONSE from the server and store in a variable called response. 
+        const response = await fetch('/api', options);
+
+        //Convert the response to a readable string and save as a variable called json.
+        const json = await response.json();
+
+        //Log the json to the chrome console to view the response from the server.
+        console.log('Response from the server below: ');
+        console.log(json.status);
+        console.log(json.latitude);
+        console.log(json.longitude);        
       });
 } else {
-    console.log('Not abalskskdfjlj');
+    console.log('Not found');
 }
